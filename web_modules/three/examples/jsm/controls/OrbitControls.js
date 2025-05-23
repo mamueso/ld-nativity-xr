eb3a3809c9cf644ab3cd0f4d5d13fffa906bc5b6
mamueso <63183599+mamueso@users.noreply.github.com>
2025-05-24 00:22:53 +0200

    typo module path

--- web_modules/three/examples/jsm/controls/OrbitControls.js
+++ web_modules/three/examples/jsm/controls/OrbitControls.js
@@ -9,7 +9,7 @@ import {
 	Plane,
 	Ray,
 	MathUtils
-} from '../../../../../web_modules/three/build/three.modules.js';
+} from '../../../../../web_modules/three/build/three.module.js';
 
 /**
  * Fires when the camera has been transformed by the controls.
