f471122fd18296535a45bebd212194fd34df1721
martin <m@marelbsi.de>
2025-05-24 17:52:04 +0200

    initial running version starting a black screen when in XR Mode

--- nscene.js
+++ nscene.js
@@ -1,5 +1,9 @@
 import './web_modules/three/build/three.min.js';
+import './web_modules/three/build/three.module.js';
 import './web_modules/seedrandom/seedrandom.min.js';
+import { OrbitControls } from './web_modules/three/examples/jsm/controls/OrbitControls.js';
+import { VRButton } from './web_modules/three/examples/jsm/webxr/VRButton.js';
+import { XRControllerModelFactory } from './web_modules/three/examples/jsm/webxr/XRControllerModelFactory.js';
 
 import { GUI } from './web_modules/three/examples/jsm/libs/dat.gui.module.js';
 import { PointerLockControls } from './web_modules/three/examples/jsm/controls/PointerLockControls.js';
@@ -111,6 +115,58 @@ const angelLightColor = 0xffffdd;
 
 var angel, star;
 
+let controlsXR;
+let container;
+let hand1, hand2;
+let controller1, controller2;
+let controllerGrip1, controllerGrip2;
+
+function initControlsXR() {
+   container = document.createElement( 'div' );
+   document.body.appendChild( container );
+
+   container.appendChild( renderer.domElement );
+   const sessionInit = { requiredFeatures: [ 'hand-tracking' ]	};
+
+   document.body.appendChild( VRButton.createButton( renderer, sessionInit ) );
+
+   controlsXR = new OrbitControls( camera, container );
+   controlsXR.target.set( 0, 1.6, 0 );
+   controlsXR.update();
+
+
+// controllers
+
+   controller1 = renderer.xr.getController( 0 );
+   scene.add( controller1 );
+
+   controller2 = renderer.xr.getController( 1 );
+   scene.add( controller2 );
+
+   const controllerModelFactory = new XRControllerModelFactory();
+//   const handModelFactory = new XRHandModelFactory();
+
+//    Hand 1
+   controllerGrip1 = renderer.xr.getControllerGrip( 0 );
+   controllerGrip1.add( controllerModelFactory.createControllerModel( controllerGrip1 ) );
+   scene.add( controllerGrip1 );
+
+   // hand1 = renderer.xr.getHand( 0 );
+   // hand1.add( handModelFactory.createHandModel( hand1 ) );
+   // scene.add( hand1 );
+
+//    Hand 2
+   controllerGrip2 = renderer.xr.getControllerGrip( 1 );
+   controllerGrip2.add( controllerModelFactory.createControllerModel( controllerGrip2 ) );
+   scene.add( controllerGrip2 );
+
+   // hand2 = renderer.xr.getHand( 1 );
+   // hand2.add( handModelFactory.createHandModel( hand2 ) );
+   // scene.add( hand2 );
+}
+
+
+
 // main entry point
 init();
 
@@ -119,6 +175,7 @@ function init() {
     initScene();
     initGUI();
     initControls();
+    initControlsXR();
     SFX.init(camera);
     applyInitialSettings();
 
